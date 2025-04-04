b0849b30acaa60c3e69bc8835b0b76a2f0e7a325
Trần Trọng Nghĩa <163334466+TranTrongNghia1609@users.noreply.github.com>
2025-04-04 10:06:32 +0700

    web socket 2

--- app/src/main/java/vn/iotstar/bt04_04_25/WebSocket/WebSocketActivity.java
+++ app/src/main/java/vn/iotstar/bt04_04_25/WebSocket/WebSocketActivity.java
@@ -1,6 +1,9 @@
 package vn.iotstar.bt04_04_25.WebSocket;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +11,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Set;
+
 import vn.iotstar.bt04_04_25.R;
 
 public class WebSocketActivity extends AppCompatActivity {
-
+    Button btnPaired;
+    ListView listDanhSach;
+    public static int REQUEST_BLUETOOTH = 1;
+    //Bluetooth
+    private BluetoothAdapter myBluetooth = null;
+    private Set<BluetoothDevice> pairedDevices;
+    public static String EXTRA_ADDRESS = "device_address";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
